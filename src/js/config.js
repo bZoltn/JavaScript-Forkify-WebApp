24855c7d13cbe326407e7e8b17d4565aef037986
bZoltn <zoltanbenchea@gmail.com>
2023-04-07 17:17:26 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8308ec18-4e4a-4bc8-bb84-c2c2a16ecbdd';
+export const MODAL_CLOSE_SEC = 2.5;
